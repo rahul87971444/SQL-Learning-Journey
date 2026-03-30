be8171231b6882e4a87c4e39d81f529559fb3a1f
Rahul Kumar <rahulkumar87971444@gmail.com>
2026-03-30 23:01:33 +0530

    Day 01: Created database and tables with constraints in SQL
    This script creates a new database and two tables with constraints.

---
+++ Day-01-Database-and-Tables/schema.sql
@@ -0,0 +1,25 @@
+-- Day 01: Database & Table Creation
+
+-- Create and use database
+CREATE DATABASE rkydb;
+USE rkydb;
+-- Create Users Table
+CREATE TABLE users (
+    userID INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    email VARCHAR(50) UNIQUE,
+    age INT CHECK (age > 18),
+    username VARCHAR(50)
+);
+-- Create Department Table
+CREATE TABLE department (
+    deptID INT PRIMARY KEY AUTO_INCREMENT,
+    deptNAME VARCHAR(50) NOT NULL
+);
+
+
+
+-- Concepts Covered:
+-- 1. CREATE DATABASE
+-- 2. CREATE TABLE
+-- 3. Constraints: PRIMARY KEY, UNIQUE, NOT NULL, CHECK
