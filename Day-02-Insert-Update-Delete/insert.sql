1fc7488be799a7c46351d5541f021b2f38e5603b
Rahul Kumar <rahulkumar87971444@gmail.com>
2026-03-31 21:33:13 +0530

    Create insert.sql for student data insertion
    Added SQL script to insert sample data into students table.

---
+++ Day-02-Insert-Update-Delete/insert.sql
@@ -0,0 +1,12 @@
+-- Insert data into students table
+
+INSERT INTO students (name, email, age, course)
+VALUES 
+('Rahul', 'rahul12@gmail.com', 20, 'BTECH'),
+('Rohit', 'rohit12@gmail.com', 22, 'BCA'),
+('Raj', 'raj3@gmail.com', 23, 'BBA');
+
+
+-- Concepts Covered:
+-- INSERT, SELECT, UPDATE, DELETE
+-- Basic CRUD operations
