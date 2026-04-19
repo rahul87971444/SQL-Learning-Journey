89d3b49d8bc625de1e048f68fe14296c850e3f54
Rahul Kumar <rahulkumar87971444@gmail.com>
2026-04-19 12:35:58 +0530

    Create Q6-Department-Highest-Salary.sql

---
+++ SQL-Questions/Q6-Department-Highest-Salary.sql
@@ -0,0 +1,15 @@
+-- Q6: Department Highest Salary
+-- LeetCode SQL
+
+SELECT d.name AS Department, e.name AS Employee, e.salary
+FROM Employee e
+JOIN Department d
+ON e.departmentId = d.id
+WHERE (e.departmentId, e.salary) IN (
+    SELECT departmentId, MAX(salary)
+    FROM Employee
+    GROUP BY departmentId
+);
+
+-- Concepts:
+-- GROUP BY, MAX, Subquery, JOIN
