1d8bd7aeff089324ae219851410e0bbe9323e64c
Rahul Kumar <rahulkumar87971444@gmail.com>
2026-05-05 20:28:45 +0530

    Delete duplicate email removal query
    Removed SQL query to delete duplicate emails based on ID.

--- SQL-Questions/Q22-Remove-Duplicate-Emails.sql
+++ SQL-Questions/Q22-Remove-Duplicate-Emails.sql
@@ -1,10 +1,1 @@
--- Q22: Remove duplicate emails (keep smallest id)
 
-DELETE p1
-FROM Person p1
-JOIN Person p2
-ON p1.email = p2.email
-AND p1.id > p2.id;
-
--- Concepts:
--- Self JOIN, DELETE, duplicates handling
