c2b5e6cca1455b58a90e809d8701d6baa249151c
Rahul Kumar <rahulkumar87971444@gmail.com>
2026-05-23 01:22:43 +0530

    Add SQL query to calculate confirmation rate

---
+++ SQL-Questions/LeetCode_SQL50/1934.ConfirmationRate/Solution.sql
@@ -0,0 +1,10 @@
+select s.user_id,ROUND(
+    IFNULL(
+        SUM(CASE when action = 'confirmed' then 1 else 0 end)/count(c.action)
+            ,0)
+            ,2) 
+            as confirmation_rate
+from Signups s
+left join Confirmations c
+on s.user_id=c.user_id
+group by s.user_id;
