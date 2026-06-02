ab93dfeab9eecbf3a02397138048db374267c6fa
Rahul Kumar <rahulkumar87971444@gmail.com>
2026-06-02 21:18:49 +0530

    Add SQL query for average selling price calculation

---
+++ SQL-Questions/LeetCode_SQL50/1251.AverageSellingPrice/Solution.sql
@@ -0,0 +1,10 @@
+select p.product_id,ROUND(
+  IFNULL(
+  SUM(p.price*u.units)/SUM(u.units)
+  ,0)
+  ,2) as average_price
+from Prices p
+left join UnitsSold u
+on p.product_id=u.product_id
+and u.purchase_date BETWEEN p.start_date AND p.end_date
+group by product_id
