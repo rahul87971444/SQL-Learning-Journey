488cee1eeac1f6ab13d2fd87f588df8271e3c0cd
Rahul Kumar <rahulkumar87971444@gmail.com>
2026-06-21 13:22:51 +0530

    Add SQL query for product sales analysis

---
+++ SQL-Questions/LeetCode_SQL50/1070.ProductSalesAnalysis/Solution.sql
@@ -0,0 +1,7 @@
+select  s.product_id, s.year as first_year,s.quantity,s.price
+from Sales s
+join (select product_id, MIN(year) as first_year
+    from Sales 
+    group by product_id)t
+on s.product_id=t.product_id
+and s.year=t.first_year;
