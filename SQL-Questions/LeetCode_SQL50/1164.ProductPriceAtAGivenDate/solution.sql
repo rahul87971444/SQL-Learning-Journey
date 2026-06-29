0b07f1a0ca37951f712913ac8ab1d8c57d4f3f2a
Rahul Kumar <rahulkumar87971444@gmail.com>
2026-06-29 23:10:09 +0530

    Add SQL solution for product price at a given date

---
+++ SQL-Questions/LeetCode_SQL50/1164.ProductPriceAtAGivenDate/solution.sql
@@ -0,0 +1,24 @@
+SELECT p.product_id,
+       IFNULL(t.new_price,10) AS price
+FROM
+(
+    SELECT DISTINCT product_id
+    FROM Products
+) p
+LEFT JOIN
+(
+    SELECT p1.product_id,
+           p1.new_price
+    FROM Products p1
+    JOIN
+    (
+        SELECT product_id,
+               MAX(change_date) AS latest_date
+        FROM Products
+        WHERE change_date <= '2019-08-16'
+        GROUP BY product_id
+    ) p2
+    ON p1.product_id = p2.product_id
+    AND p1.change_date = p2.latest_date
+) t
+ON p.product_id = t.product_id;
